213fd0d4f7699e94da4ef04e3e96563997013b88
savaleinart <savaleinart@gmail.com>
2021-05-18 15:31:48 +0200

    Découpage de la requête et mineType

--- src/main/java/fr/ul/miage/Client.java
+++ src/main/java/fr/ul/miage/Client.java
@@ -23,4 +23,44 @@ public class Client extends Thread{
             e.printStackTrace();
         }
     }
+
+    public String getfilename(String requete){
+        String filename = "sites/";
+        StringTokenizer st = new StringTokenizer(requete);
+        try{
+            if (st.hasMoreElements() && st.nextToken().equalsIgnoreCase("GET") && st.hasMoreElements()){
+                filename += st.nextToken();
+            }
+            else{
+                throw new FileNotFoundException(); // Bad request
+            }
+            //si le filename fini par "/" alors on doit accès au index.html du chemin
+            if (filename.endsWith("/")){
+                filename += "index.html";
+            }
+            //pour enlever tous les "/" au début du filename
+            while(filename.indexOf("/") == 0){
+                filename = filename.substring(1);
+            }
+            //pour remplacer les "/" par des "\"
+            filename = filename.replace('/', File.separator.charAt(0));
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return filename;
+    }
+
+    public String getMimeType(String filename){
+        String mimeType = "text/plain";
+        if (filename.endsWith(".html") || filename.endsWith(".htm"))
+            mimeType = "text/html";
+        else if (filename.endsWith(".png"))
+            mimeType = "image/png";
+        else if (filename.endsWith(".js"))
+            mimeType = "application/javascript";
+        else if (filename.endsWith(".css"))
+            mimeType = "text/css";
+        return mimeType;
+    }
 }
