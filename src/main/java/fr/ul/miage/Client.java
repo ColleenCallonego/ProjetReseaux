d571dd5bce1d4333efba074f7b243c4348a22a93
ColleenCallonego <colleencallonego@gmail.com>
2021-05-18 08:57:22 +0200

    Récupération de la requête

--- src/main/java/fr/ul/miage/Client.java
+++ src/main/java/fr/ul/miage/Client.java
@@ -1,4 +1,26 @@
 package fr.ul.miage;
 
-public class Client {
+import java.io.*;
+import java.net.Socket;
+
+public class Client extends Thread{
+    public Socket socket;
+
+    public Client(Socket s){
+        this.socket = s;
+        start();
+    }
+
+    public void run(){
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream out = new PrintStream(new BufferedOutputStream(socket.getOutputStream()));
+
+            //récupération de la requête
+            String s = in.readLine();
+            System.out.println(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
